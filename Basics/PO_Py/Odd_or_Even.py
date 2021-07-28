47e67f161489ef0414f3301082b53b9a69666ee1
subashshakya <subashshakya12@gmail.com>
2021-07-28 15:34:50 +0545

    Conditional statement

---
+++ Basics/PO_Py/Odd_or_Even.py
@@ -0,0 +1,7 @@
+# using conditional blocks to check if the entered number is odd or even
+Num = int(input("Enter any number you want!"))
+if Num > 0:
+    if Num % 2 == 0:
+        print("The number is even")
+    else:
+        print("The entered number is odd")
