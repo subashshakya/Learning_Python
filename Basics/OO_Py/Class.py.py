203b35643e2f8fafacff4aeb60ba6715a5496662
subashshakya <subashshakya12@gmail.com>
2021-07-28 15:38:04 +0545

    Class and Object in Py

---
+++ Basics/OO_Py/Class.py.py
@@ -0,0 +1,12 @@
+class Employee:
+    def __init__(self, memberID, name, salary):
+        self.memberID = MemberID
+        self.name = Name
+        self.salary = Salary
+
+    def Display_Emp(self):
+        print("Name of the Employee is " + self.Name)
+        print("The member ID of the Employee is: " + self.MemberID)
+        print("The salary of the employee is: " + self.Salary)
+
+
